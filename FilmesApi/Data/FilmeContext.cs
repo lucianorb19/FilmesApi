73e5d28cba17ddf8390a875ccd7fd60f2f7ef4ac
lucianorb19 <52081214+lucianorb19@users.noreply.github.com>
2025-04-11 14:37:35 -0300

    DataBase Connection - beginning

---
+++ FilmesApi/Data/FilmeContext.cs
@@ -0,0 +1,21 @@
+﻿using FilmesApi.Models;
+using Microsoft.EntityFrameworkCore;//DbContext
+
+namespace FilmesApi.Data
+{
+    public class FilmeContext : DbContext//CLASSE QUE REMEDIA LIGAÇÃO FILME (ENTIDADE) <-> FILME (BD)
+    {
+        //PROPRIEDADES
+
+        //PROPRIEDADE DE ACESSO AOS FILMES DA BD
+        public DbSet<Filme> Filmes{ get; set; }
+
+
+        //CONSTRUTOR QUE USA O CONSTRUTOR BASE (CONSTRUTOR DE DbContext)
+        public FilmeContext(DbContextOptions<FilmeContext> opts)
+        : base(opts)
+        {               
+               
+        }
+    }
+}
